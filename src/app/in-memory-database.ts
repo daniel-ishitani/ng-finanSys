ff89991ecdf3ed8850894aaf5abfa0d38dd70a34
daniel-ishitani <ishitanidim@gmail.com>
2020-01-07 21:35:42 -0300

    added in memory database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories = [
+      { id: 1, name: "Leisure", description: "Cinema, parks, beach, ..."},
+      { id: 2, name: "Health", description: "Health care"},
+      { id: 3, name: "Salary", description: "Salary"},
+      { id: 4, name: "Freelas", description: "Extraordinary income"}
+    ];
+
+    return categories;
+  }
+}
