c15b8b587e776da7726ce64cd9d9f8ba5ae025c9
daniel-ishitani <ishitanidim@gmail.com>
2020-01-09 23:14:22 -0300

    added entry list component

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
 
 import { Category } from './pages/categories/shared/category.model';
+import { Entry } from './pages/entries/shared/entry.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
@@ -11,6 +12,11 @@ export class InMemoryDatabase implements InMemoryDbService {
       { id: 4, name: "Freelas", description: "Extraordinary income"}
     ];
 
-    return {categories};
+    const entries: Entry[] = [
+      { id: 1, name: 'Magic', categoryId: categories[0].id, category: categories[0], paid: true, date: '01/01/2020', amount: '100,00', kind: 'expense', description: 'pre release'} as Entry,
+      { id: 2, name: 'Magic', categoryId: categories[0].id, category: categories[0], paid: true, date: '01/01/2020', amount: '100,00', kind: 'expense', description: 'pre release'} as Entry
+    ]
+
+    return {categories, entries};
   }
 }
---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { EntryService } from '../shared/entry.service';
+import { Entry } from '../shared/entry.model';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.scss']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(
+    private entryService: EntryService
+  ) { }
+
+  ngOnInit() {
+    this.entryService.getAll().subscribe(
+      entries => this.entries = entries,
+      error => console.log(error)
+    );
+  }
+
+  onDeleteEntry(entry) {
+    const mustDelete = confirm('Do you really wish to delete this item?')
+
+    if(mustDelete) {
+      this.entryService.delete(entry.id).subscribe(
+        () => this.entries = this.entries.filter(element => element != entry),
+        error => console.log(error)
+      );
+    }
+  }
+  
+}
