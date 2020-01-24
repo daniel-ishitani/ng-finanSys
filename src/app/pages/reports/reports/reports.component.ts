ac531446273b966df672e35cdb1890d4bda34d14
daniel-ishitani <ishitanidim@gmail.com>
2020-01-23 21:02:19 -0300

    added reports template and edit page-header component

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  months = [
+    { value: '1', name: 'January'},
+    { value: '2', name: 'February'},
+    { value: '3', name: 'March'},
+    { value: '4', name: 'April'},
+    { value: '5', name: 'May'},
+    { value: '6', name: 'June'},
+    { value: '7', name: 'July'},
+    { value: '8', name: 'August'},
+    { value: '9', name: 'September'},
+    { value: '10', name: 'October'},
+    { value: '11', name: 'November'},
+    { value: '12', name: 'December'},
+  ];
+
+  years = [
+    { value: '2016', name: '2016'},
+    { value: '2017', name: '2017'},
+    { value: '2018', name: '2018'},
+    { value: '2019', name: '2019'},
+    { value: '2020', name: '2020'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onGenerateReports() {
+    
+  }
 }
